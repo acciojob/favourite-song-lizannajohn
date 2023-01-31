d51ba1edaa15823cf19eb0b33c294abc5b3b5851
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-31 21:01:10 +0400

    fav song solved

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let div = document.createElement("div")
+div.setAttribute("class", "divtag")
+// console.log(div);
+
+div.innerHTML = '<a href="https://youtu.be/aMZE2hmRnhE"> My current favourite song is Wait by M83 </a>'
+document.body.appendChild(div);
